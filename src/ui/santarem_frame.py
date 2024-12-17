8ac48a103df64a52a7dc7c21f9426209b6a0d6cb
BonomoJoaoPaulo <jpabonomo@gmail.com>
2024-12-17 13:11:08 -0300

    feat: Santarem advanced options done.

--- src/ui/santarem_frame.py
+++ src/ui/santarem_frame.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from utils.santarem_functions import compare_quantities_by_goods
+from utils.santarem_functions import compare_quantities_by_goods, compare_quantities_by_goods_simplificado, compare_arrivals_over_time, compare_status_distribution, compare_quantities_by_status
 
 def create_santarem_frame(container, show_frame, menu_frame):
     santarem_frame = tk.Frame(container)
@@ -11,8 +11,18 @@ def create_santarem_frame(container, show_frame, menu_frame):
     santarem_option1 = tk.Button(santarem_frame, text="Quantidades por Mercadoria", font=("Arial", 12), command=compare_quantities_by_goods)
     santarem_option1.pack(pady=5)
 
-    santarem_option2 = tk.Button(santarem_frame, text="Opção B - Outra coisa", font=("Arial", 12), command=lambda: print("Executando Opção B"))
+    santarem_option5 = tk.Button(santarem_frame, text="Operações por Mercadoria Simplificadas", font=("Arial", 12), command=compare_quantities_by_goods_simplificado)
+    santarem_option5.pack(pady=5)
+    
+    santarem_option2 = tk.Button(santarem_frame, text="Linha Temporal", font=("Arial", 12), command=compare_arrivals_over_time)
     santarem_option2.pack(pady=5)
+    
+    santarem_option3 = tk.Button(santarem_frame, text="Distribuição de Status", font=("Arial", 12), command=compare_status_distribution)
+    santarem_option3.pack(pady=5)
+    
+    santarem_option4 = tk.Button(santarem_frame, text="Quantidades por Status", font=("Arial", 12), command=compare_quantities_by_status)
+    santarem_option4.pack(pady=5)
+    
 
     # Botões de navegação
     btn_back_to_menu = tk.Button(santarem_frame, text="Voltar ao menu principal", font=("Arial", 12), command=lambda: show_frame(menu_frame))
